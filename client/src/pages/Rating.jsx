e173fb622697a473e8ad27c7932caa58317e317e
Mohammed Al-Muqsit <almuqsit101@gmail.com>
2024-12-09 08:40:52 -0800

    Added functionality for rating user and seller and updating transaction table so that it checks if the buyer and seller are rated. if so, then they will not show up after rating for that transaction

--- client/src/pages/Rating.jsx
+++ client/src/pages/Rating.jsx
@@ -1,20 +1,182 @@
 import React from 'react'
-import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
-import Auth from './Authentication'
 import { useEffect } from 'react'
 import { useState } from 'react'
 
 const Rating = () => {
-    const [pendingUsers, setPendingUsers] = useState([]);
+    //const [pendingUsers, setPendingUsers] = useState([]);
+    const [pendingBuyers, setPendingBuyers] = useState([]);
+    const [pendingSellers, setPendingSellers] = useState([]);
+    const [userDetails, setUserDetails] = useState(null);
+    const [ratingBuyer, setRatingBuyer] = useState(new Map());
+    const [ratingSeller, setRatingSeller] = useState(new Map());
+    //get transaction id ==> check if buyer_id == user
 
-    // Fetch pending users on component mount
+  // Fetch logged-in user details
+    const handleRatingChangeBuyer = (transactionId, buyerId, rating) => {
+    setRatingBuyer((prev) => {
+        // Copy the previous Map to avoid modifying state directly
+        const updatedRatings = new Map(prev);
+        updatedRatings.set(transactionId, { buyerId, rating }); // Map transaction_id to an object with buyerId and rating
+        return updatedRatings; // Return the updated Map
+            });
+    };
+
+    const handleRatingChangeSeller = (transactionId, sellerId, rating) => {
+    setRatingSeller((prev) => {
+        // Copy the previous Map to avoid modifying state directly
+        const updatedRatings = new Map(prev);
+        updatedRatings.set(transactionId, { sellerId, rating }); // Map transaction_id to an object with sellerId and rating
+        return updatedRatings; // Return the updated Map
+    });
+};
+
+ 
+
+    const handleRateUsers = async() => {
+        // Send the ratings to the server
+        // One for buyer and one for seller
+        const ratingsData = Array.from(ratingBuyer.entries()).map(([transactionId, { buyerId, rating }, ]) => ({
+            transaction_id: transactionId,
+            rater_id: userDetails.id,   // The logged-in user's ID (rater)
+            ratee_id: buyerId,            // The ID of the buyer/seller
+            rating: rating,        // The rating given by the rater
+        }));
+        const ratingsData2 = Array.from(ratingSeller.entries()).map(([transactionId, { sellerId, rating }, ]) => ({
+            transaction_id: transactionId,
+            rater_id: userDetails.id,   // The logged-in user's ID (rater)
+            ratee_id: sellerId,            // The ID of the buyer/seller
+            rating: rating,        // The rating given by the rater
+        }));
+        //console.log('Sending ratings:', ratingsData); // Check if data is correct
+        try {
+            const response = await fetch('http://localhost:3001/ratings', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(ratingsData), // Sending the ratings data
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.error || 'Failed to send buyer ratings.');
+            }
+
+            const data = await response.json();
+            console.log('Ratings sent successfully for buyer:', data);
+        } catch (err) {
+            console.error('Error sending buyer ratings:', err);
+        }
+
+        try {
+            const response = await fetch('http://localhost:3001/ratings', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(ratingsData2), // Sending the ratings data
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.error || 'Failed to send seller ratings.');
+            }
+
+            const data = await response.json();
+            console.log('Ratings sent successfully for seller:', data);
+            fetchTransactionBuyerDetails();
+            fetchTransactionSellerDetails();
+        } catch (err) {
+            console.error('Error sending seller ratings:', err);
+        }
+    }; 
+    const fetchUserDetails = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/ratings/user', {
+          method: 'GET',
+          credentials: 'include', // Ensures the session cookie is sent with the request
+        });
+
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(errorData.error || 'Failed to fetch user details.');
+        }
+
+        const data = await response.json();
+        setUserDetails(data);
+        //console.log('Logged-in user ID using first data.id:', data.id); // Log the user ID
+        //console.log(data);
+        //console.log('Logged-in user details:', userDetails);
+      } catch (err) {
+        //console.error('Error fetching user details:', err);
+      }
+    };
+
     useEffect(() => {
-        fetch('http://localhost:3001/users')
-        .then((res) => res.json())
-        .then((data) => setPendingUsers(data))
-        .catch((err) => console.error('Error fetching users:', err));
+        fetchUserDetails();
     }, []);
+        
+  
+
+  //console.log('Logged-in user ID:', userDetails?.id);
+
+
+    // Fetch transaction details for buyers
+  const fetchTransactionBuyerDetails = async () => {
+    if (!userDetails?.id) return; // Ensure userDetails is available
+    try {
+      const response = await fetch(`http://localhost:3001/transactions/buyers/${userDetails.id}`, {
+        method: 'GET',
+        credentials: 'include', // Ensures the session cookie is sent with the request
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error || 'Failed to fetch transaction details.');
+      }
+
+      const data = await response.json();
+      setPendingBuyers(data);
+    } catch (err) {
+      console.error('Error fetching transaction details:', err);
+    }
+  };
+
+    useEffect(() => {
+        if (userDetails?.id) {
+            fetchTransactionBuyerDetails();
+        }
+        }, [userDetails]);
+    
+    const fetchTransactionSellerDetails = async () => {
+    if (!userDetails?.id) return; // Ensure userDetails is available
+        try {
+        const response = await fetch(`http://localhost:3001/transactions/sellers/${userDetails.id}`, {
+            method: 'GET',
+            credentials: 'include', // Ensures the session cookie is sent with the request
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.error || 'Failed to fetch transaction details.');
+        }
+
+        const data = await response.json();
+        setPendingSellers(data);
+        } catch (err) {
+        console.error('Error fetching transaction details:', err);
+        }
+  };
+
+    useEffect(() => {
+        if (userDetails?.id) {
+            fetchTransactionSellerDetails();
+        }
+        }, [userDetails]);
+    
+
+
     return (
         <>
             <Navbar />
@@ -29,18 +191,26 @@ const Rating = () => {
                                 <th className="border border-gray-300 p-2 text-left">Name</th>
                                 <th className="border border-gray-300 p-2 text-left">Username</th>
                                 <th className="border border-gray-300 p-2 text-left">Email</th>
+                                <th className="border border-gray-300 p-2 text-left">Transaction ID</th>
                                 <th className="border border-gray-300 p-2 text-center">Rate</th>
                             </tr>
                         </thead>
 
                         <tbody>
-                        {pendingUsers.map((user) => (
-                            <tr key={user.id} className="bg-gray-50">
+                        {pendingBuyers.map((user) => (
+                            <tr key={user.transaction_id} className="bg-gray-50">
                                 <td className="border border-gray-300 p-2">{user.name}</td>
                                 <td className="border border-gray-300 p-2">{user.username}</td>
                                 <td className="border border-gray-300 p-2">{user.email}</td>
+                                <td className="border border-gray-300 p-2">{user.transaction_id}</td>
                                 <td className="border border-gray-300 p-2 text-center space-x-2">
-                                    <select defaultValue={0}>
+                                    <select defaultValue={0}
+                                        onChange={(e) => {
+                                            //console.log(e.target.value, user.id);
+                                            handleRatingChangeBuyer(user.transaction_id, user.id, e.target.value);
+                                            //console.log(userDetails.id, userDetails.username);
+                                            
+                                        }} >    
                                         <option value={0} disabled></option>
                                         <option value = {1}>1</option>
                                         <option value = {2}>2</option>
@@ -51,7 +221,7 @@ const Rating = () => {
                                 </td>
                             </tr>
                         ))}
-                        {pendingUsers.length === 0 && (
+                        {pendingBuyers.length === 0 && (
                             <tr>
                             <td
                                 colSpan="4"
@@ -72,18 +242,21 @@ const Rating = () => {
                                 <th className="border border-gray-300 p-2 text-left">Name</th>
                                 <th className="border border-gray-300 p-2 text-left">Username</th>
                                 <th className="border border-gray-300 p-2 text-left">Email</th>
+                                <th className="border border-gray-300 p-2 text-left">Transaction ID</th>
                                 <th className="border border-gray-300 p-2 text-center">Rate</th>
                             </tr>
                         </thead>
 
                         <tbody>
-                        {pendingUsers.map((user) => (
-                            <tr key={user.id} className="bg-gray-50">
+                        {pendingSellers.map((user) => (
+                            <tr key={user.transaction_id} className="bg-gray-50">
                                 <td className="border border-gray-300 p-2">{user.name}</td>
                                 <td className="border border-gray-300 p-2">{user.username}</td>
                                 <td className="border border-gray-300 p-2">{user.email}</td>
+                                <td className="border border-gray-300 p-2">{user.transaction_id}</td>
                                 <td className="border border-gray-300 p-2 text-center space-x-2">
-                                    <select defaultValue={0}>
+                                    <select defaultValue={0}
+                                        onChange={(e) => handleRatingChangeSeller(user.transaction_id, user.id, e.target.value)}>
                                         <option value={0} disabled></option>
                                         <option value = {1}>1</option>
                                         <option value = {2}>2</option>
@@ -94,7 +267,7 @@ const Rating = () => {
                                 </td>
                             </tr>
                         ))}
-                        {pendingUsers.length === 0 && (
+                        {pendingSellers.length === 0 && (
                             <tr>
                             <td
                                 colSpan="4"
@@ -106,6 +279,11 @@ const Rating = () => {
                         )}
                         </tbody>
                         </table >
+
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6"
+                            onClick={handleRateUsers}>
+                            Rate Users
+                        </button>
                     </div>
                     </div>
 
--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const usersRouter = require('./routes/Users'); // Import the users router
 const adminRouter = require('./routes/Admin'); // Import the admin router
 const bidRouter = require('./routes/Bids'); // Import the bids router
 const listingsRouter = require('./routes/Listings');
-
+const ratingRouter = require('./routes/Rating');
 const app = express();
 
 // Middleware
@@ -30,6 +30,7 @@ app.use(usersRouter);
 app.use(adminRouter);
 app.use('/listings', listingsRouter);
 app.use(bidRouter);
+app.use(ratingRouter);
 
 // Start the server
 app.listen(3001, () => {
